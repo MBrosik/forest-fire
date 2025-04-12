d45e133964e0eb80b0d760b7c237e569480649e3
MBrosik <michal.bros1@gmail.com>
2025-04-12 11:47:04 +0200

    Remove unnecessary files

--- fire-visualization/src/components/simulationPanel/RecomendedDecisions.tsx
+++ fire-visualization/src/components/simulationPanel/RecomendedDecisions.tsx
@@ -7,12 +7,12 @@ const RecommendedDecisions = () => {
          <Divider><Typography variant="h2">Recommended Decision</Typography> </Divider>         
          <List sx={{display: 'grid', gridTemplateColumns: '1fr'}}>
             <ListItem sx={{display: 'grid', width:"100%", gridTemplateColumns: '400px 100px 100px', gap:5}}>               
-               <Typography>Firebreak width: 5m</Typography>
+               <Typography>Action 1</Typography>
                <Button variant="contained" color="success">Apply</Button>
                <Button variant="contained" color="error">Cancel</Button>
             </ListItem>
             <ListItem sx={{display: 'grid',  gridTemplateColumns: '400px 100px 100px', gap:5}}>               
-               <Typography>Firebreak width: </Typography>
+               <Typography>Action 2 </Typography>
                <Button variant="contained" color="success">Apply</Button>
                <Button variant="contained" color="error">Cancel</Button>
             </ListItem>
